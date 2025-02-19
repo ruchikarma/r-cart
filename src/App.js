e8f33ce3f74cb50f359c99a66aef4aef910cda2f
ruchikarma <ruchiscaler@gmail.com>
2025-02-19 23:50:45 +0530

    chg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ruhci <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <Sidebar />
+   </div>
   );
 }
 
