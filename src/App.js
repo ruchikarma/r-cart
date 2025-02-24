0ef5986441a2f6fd2e609b6b2f4dcc470e9f5e8a
ruchikarma <ruchiscaler@gmail.com>
2025-02-24 23:11:15 +0530

    pages routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import WomenPage from "./pages/WomenPage";
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,82 +1,83 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    variants: [
-      {
-        props: ({ open }) => open,
-        style: {
-          transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-          }),
-          marginLeft: 0,
-        },
-      },
-    ],
-  }),
-);
+// const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+//   ({ theme }) => ({
+//     flexGrow: 1,
+//     padding: theme.spacing(3),
+//     transition: theme.transitions.create("margin", {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//     marginLeft: `-${drawerWidth}px`,
+//     variants: [
+//       {
+//         props: ({ open }) => open,
+//         style: {
+//           transition: theme.transitions.create("margin", {
+//             easing: theme.transitions.easing.easeOut,
+//             duration: theme.transitions.duration.enteringScreen,
+//           }),
+//           marginLeft: 0,
+//         },
+//       },
+//     ],
+//   })
+// );
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: `${drawerWidth}px`,
-        transition: theme.transitions.create(['margin', 'width'], {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-    },
-  ],
-}));
+// const AppBar = styled(MuiAppBar, {
+//   shouldForwardProp: (prop) => prop !== "open",
+// })(({ theme }) => ({
+//   transition: theme.transitions.create(["margin", "width"], {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.leavingScreen,
+//   }),
+//   variants: [
+//     {
+//       props: ({ open }) => open,
+//       style: {
+//         width: `calc(100% - ${drawerWidth}px)`,
+//         marginLeft: `${drawerWidth}px`,
+//         transition: theme.transitions.create(["margin", "width"], {
+//           easing: theme.transitions.easing.easeOut,
+//           duration: theme.transitions.duration.enteringScreen,
+//         }),
+//       },
+//     },
+//   ],
+// }));
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
-}));
+// const DrawerHeader = styled("div")(({ theme }) => ({
+//   display: "flex",
+//   alignItems: "center",
+//   padding: theme.spacing(0, 1),
+//   // necessary for content to be below app bar
+//   ...theme.mixins.toolbar,
+//   justifyContent: "flex-end",
+// }));
 
-function Sidebar() {
+const Sidebar = () => {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
@@ -88,56 +89,42 @@ function Sidebar() {
     setOpen(false);
   };
 
+
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar position="fixed" open={open}>
+      <MuiAppBar position="fixed" sx={{ transition: "margin 0.3s", marginLeft: open ? drawerWidth : 0 }}>
         <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={[
-              {
-                mr: 2,
-              },
-              open && { display: 'none' },
-            ]}
-          >
+          <IconButton color="inherit" onClick={handleDrawerOpen} edge="start" sx={{ mr: 2, display: open ? "none" : "block" }}>
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            R MART
+          <Typography variant="h6" noWrap>
+            RUCHI MART
           </Typography>
         </Toolbar>
-      </AppBar>
+      </MuiAppBar>
       <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
+        sx={{ width: drawerWidth, flexShrink: 0, "& .MuiDrawer-paper": { width: drawerWidth } }}
         variant="persistent"
         anchor="left"
         open={open}
       >
-        <DrawerHeader>
+        <div>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            {theme.direction === "ltr" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
           </IconButton>
-        </DrawerHeader>
+        </div>
         <Divider />
         <List>
-          {['Women', 'Mens', 'Shoes', 'Perfume'].map((text, index) => (
+          {[
+            { text: "Women", path: "/women" },
+            { text: "Men", path: "/men" },
+            { text: "Shoes", path: "/shoes" },
+            { text: "Perfume", path: "/perfume" }
+          ].map(({ text, path }, index) => (
             <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
+              <ListItemButton component={Link} to={path}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
@@ -145,50 +132,129 @@ function Sidebar() {
         </List>
         <Divider />
         <List>
-          {['Wishlist', 'Cart', 'Checkout'].map((text, index) => (
+          {[
+            { text: "Wishlist", path: "/wishlist" },
+            { text: "Cart", path: "/cart" },
+            { text: "Checkout", path: "/checkout" }
+          ].map(({ text, path }, index) => (
             <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
+              <ListItemButton component={Link} to={path}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
       </Drawer>
-      <Main open={open}>
-        <DrawerHeader />
-        <Typography sx={{ marginBottom: 2 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography sx={{ marginBottom: 2 }}>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Main>
     </Box>
   );
-}
+  // return (
+  //   <Box sx={{ display: "flex" }}>
+  //     <CssBaseline />
+  //     <AppBar position="fixed" open={open}>
+  //       <Toolbar>
+  //         <IconButton
+  //           color="inherit"
+  //           aria-label="open drawer"
+  //           onClick={handleDrawerOpen}
+  //           edge="start"
+  //           sx={[
+  //             {
+  //               mr: 2,
+  //             },
+  //             open && { display: "none" },
+  //           ]}
+  //         >
+  //           <MenuIcon />
+  //         </IconButton>
+  //         <Typography variant="h6" noWrap component="div">
+  //           R MART
+  //         </Typography>
+  //       </Toolbar>
+  //     </AppBar>
+  //     <Drawer
+  //       sx={{
+  //         width: drawerWidth,
+  //         flexShrink: 0,
+  //         "& .MuiDrawer-paper": {
+  //           width: drawerWidth,
+  //           boxSizing: "border-box",
+  //         },
+  //       }}
+  //       variant="persistent"
+  //       anchor="left"
+  //       open={open}
+  //     >
+  //       <DrawerHeader>
+  //         <IconButton onClick={handleDrawerClose}>
+  //           {theme.direction === "ltr" ? (
+  //             <ChevronLeftIcon />
+  //           ) : (
+  //             <ChevronRightIcon />
+  //           )}
+  //         </IconButton>
+  //       </DrawerHeader>
+  //       <Divider />
+  //       <List>
+  //         {["Women", "Mens", "Shoes", "Perfume"].map((text, index) => (
+  //           <ListItem key={text} disablePadding>
+  //             <ListItemButton>
+  //               <ListItemIcon>
+  //                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+  //               </ListItemIcon>
+  //               <ListItemText primary={text} />
+  //             </ListItemButton>
+  //           </ListItem>
+  //         ))}
+  //       </List>
+  //       <Divider />
+  //       <List>
+  //         {["Wishlist", "Cart", "Checkout"].map((text, index) => (
+  //           <ListItem key={text} disablePadding>
+  //             <ListItemButton>
+  //               <ListItemIcon>
+  //                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+  //               </ListItemIcon>
+  //               <ListItemText primary={text} />
+  //             </ListItemButton>
+  //           </ListItem>
+  //         ))}
+  //       </List>
+  //     </Drawer>
+  //     <Main open={open}>
+  //       <DrawerHeader />
+  //       <Typography sx={{ marginBottom: 2 }}>
+  //         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  //         eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+  //         dolor purus non enim praesent elementum facilisis leo vel. Risus at
+  //         ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+  //         quisque non tellus. Convallis convallis tellus id interdum velit
+  //         laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+  //         adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+  //         integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+  //         eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+  //         quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+  //         vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+  //         lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+  //         faucibus et molestie ac.
+  //       </Typography>
+  //       <Typography sx={{ marginBottom: 2 }}>
+  //         Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+  //         ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+  //         elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+  //         sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+  //         mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+  //         risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+  //         purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+  //         tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+  //         morbi tristique senectus et. Adipiscing elit duis tristique
+  //         sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+  //         eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+  //         posuere sollicitudin aliquam ultrices sagittis orci a.
+  //       </Typography>
+  //     </Main>
+  //   </Box>
+  // );
+};
 
 export default Sidebar;
