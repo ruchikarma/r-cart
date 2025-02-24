7bfe2a9f0bf50319ebe3329a4766fc568a1aabc5
ruchikarma <ruchiscaler@gmail.com>
2025-02-24 22:31:03 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import WomenPage from "./pages/WomenPage";
+import MenPage from "./pages/MenPage";
+import ShoesPage from "./pages/ShoesPage";
+import PerfumePage from "./pages/PerfumePage";
+import WishlistPage from "./pages/WishlistPage";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+
 import './App.css';
 
 function App() {
   return (
-   <div>
+    <Router>
     <Sidebar />
-   </div>
+    <Routes>
+      <Route path="/women" element={<WomenPage />} />
+      <Route path="/men" element={<MenPage />} />
+      <Route path="/shoes" element={<ShoesPage />} />
+      <Route path="/perfume" element={<PerfumePage />} />
+      <Route path="/wishlist" element={<WishlistPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+    </Routes>
+  </Router>
   );
 }
 
