d7be24a48caaf227cf3a70fd3b452eac53e1182a
ruchikarma <ruchiscaler@gmail.com>
2025-03-27 11:09:15 +0530

    approutes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
-import {WomenPage}from "./pages/WomenPage";
-import {MenPage} from "./pages/MenPage";
-import {ShoesPage} from "./pages/ShoesPage";
+import WomenPage from "./pages/WomenPage";
+import MenPage from "./pages/MenPage";
+import ShoesPage from "./pages/ShoesPage";
 import PerfumePage from "./pages/PerfumePage";
 import WishlistPage from "./pages/WishlistPage";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
 
 import './App.css';
-// import { AuthProvider } from './components/AuthContext';
 
 function App() {
   return (
@@ -26,6 +25,7 @@ function App() {
       <Route path="/checkout" element={<CheckoutPage />} />
     </Routes>
   </Router>
+
   );
 }
 
